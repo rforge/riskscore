c008377caad83c5de7604320fefa7df076c7e4c8
tag <tag@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-15 12:56:41 +0000

    new files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskscore@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ModelGood/R/summary.predictStatusProb.R
@@ -0,0 +1,7 @@
+#' @S3method summary Roc
+summary.predictStatusProb <- function(object,cuts=seq(0,100,25),...){
+    table("Predicted risk"=cut(round(100*object,1),
+              cuts,
+              include.lowest=TRUE,
+              labels=paste(paste(cuts[-length(cuts)],cuts[-1],sep="-"),"%",sep="")))
+}
